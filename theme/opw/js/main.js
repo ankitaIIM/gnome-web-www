9169f2338da3b4c5f6915cd04caffba9dd0cc9b7
saumyad <saumyad@src.gnome.org>
2013-07-15 23:30:55 +0530

    Responsive OPW

--- theme/opw/js/main.js
+++ theme/opw/js/main.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function($) {
-    $(".scroll").click(function(event){		
+    $(".scroll, .scroll-logo").click(function(event){
         event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top - 60}, 500);
+        $('html,body').animate({scrollTop:$(this.hash).offset().top - ($('#home').innerHeight() - $('#home').height()) + 10}, 500);
     });
-    
+
     var aboveHeight = $('header').outerHeight();
     $(window).scroll(function(){
         $('.nav-container').addClass('fixed').css('top','0').next()
         .css('padding-top', '80px');
     });
-    
+
     $('.participants-list').hide();
     $('.view-participants').click(function () {
         if( $('.participants-list').is(":hidden") ) {
@@ -20,4 +20,27 @@ jQuery(document).ready(function($) {
             $('.participants-list').hide(400);
         }
     });
+});
+
+$(function() {
+    var pull        = $('#pull');
+    menu        = $('.nav');
+    menuHeight  = menu.height();
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+});
+
+$(window).resize(function(){
+    var w = $(window).width();
+    if(w > 720 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    }
+});
+
+$(function() {
+    $('.nav li a, .scroll-logo').on('click',function(){
+        $('.nav-collapse').collapse('hide');
+    }); 
 });
